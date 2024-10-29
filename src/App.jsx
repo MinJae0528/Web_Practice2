314c01905471e79e6d09809441f2771212675bf8
MinJae0528 <minj7292@naver.com>
2024-10-29 20:02:38 +0900

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="Header">
+        <a href="https://www.youtube.com/" target="_blank">
+          <img src= "/Youtube-logo.jpg" className="logo" alt="Youtube logo" />
+        </a>
+      </div>
+      <h1>Youtube</h1>
+    </>
+  )
+}
+
+export default App
