1378370d4abd7103a9e1024c580598cafa7a0624
MinJae0528 <minj7292@naver.com>
2024-10-29 22:39:13 +0900

    Add) 여러 아이콘과 jsx, css 파일 추가 상단 거의 마무리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import Youtube from './Youtube';
 
 function App() {
-
   return (
-    <>
-      <div id="Header">
-        <a href="https://www.youtube.com/" target="_blank">
-          <img src= "/Youtube-logo.jpg" className="logo" alt="Youtube logo" />
-        </a>
-      </div>
-      <h1>Youtube</h1>
-    </>
-  )
+    <div>
+      <Youtube />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Youtube.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Youtube.css';
+
+const Youtube = () => {
+    return (
+        <div className="Youtube">
+            <header className="Header">
+                <div className="MenuAndLogo">
+                    <button className="Menu"><img src="/Menu-icon.png"></img></button>
+                    <a href="https://www.youtube.com/" target="_blank">
+                    <img src= "/Youtube-TextLogo.jpg" className="logo" alt="Youtube logo" />
+                    </a>
+                </div>
+                <div className="Search">
+                    <div className="SearchContainer">
+                        <input type="text" className="txtSearch"></input>
+                        <button className="SearachButton">
+                            <img src="/Search-icon.png" alt="검색" />
+                        </button>
+                    </div>
+                    <button className="MicButton">
+                        <img src="/Mic-icon.jpg" />
+                    </button>
+                </div>
+                <div className="Setting">
+                    <button className="CreateButton">
+                        <img src="VideoCreate-icon.png" />
+                    </button>
+                    <button className="AlarmButton">
+                        <img src="YoutubeAlarm-icon.png" />
+                    </button>
+                    <button className="UserButton">
+                        <img src="User-icon.png" />
+                    </button>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Youtube;
